208a862457bfc31feb90aaf48c8fe4ed256b902c
gm12180 <lesleymukami@gmail.com>
2022-08-23 11:45:06 -0700

    tsk5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    if i <= 9:
-        print(0, end="")
-        print("{}".format(i), end=", " if i < 98 else ', {}\n'.format(99))
+for n in range(100):
+    print("{n:02d}".format(n=n), end=(', ' if n < 99 else '\n'))
